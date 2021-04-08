f3c42c9007fbbd54627801b7437d92722d091e8e
Brennan35 <77704776+Brennan35@users.noreply.github.com>
2021-04-07 19:05:49 -0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Kruffalo is online');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login('ODI5NDg4MzAzMDc3NzIwMDY1.YG43Lg.g0F-Ka-f3oNgwYIym7PleKKvxPw')
+
+
